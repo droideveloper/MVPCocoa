6e1f1d79859b304b72f259448b57553d385d58bf
droideveloper <fatih.sen.boun@gmail.com>
2018-11-25 11:43:33 +0300

    updat

---
+++ MVPCocoa/MVPCocoa.h
@@ -0,0 +1,19 @@
+//
+//  MVPCocoa.h
+//  MVPCocoa
+//
+//  Created by Fatih Şen on 25.11.2018.
+//  Copyright © 2018 Fatih Şen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVPCocoa.
+FOUNDATION_EXPORT double MVPCocoaVersionNumber;
+
+//! Project version string for MVPCocoa.
+FOUNDATION_EXPORT const unsigned char MVPCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVPCocoa/PublicHeader.h>
+
+
