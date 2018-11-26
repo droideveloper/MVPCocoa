f27c213f63fe6bdb8746744f1cb8979b3c1ab6ef
droideveloper <fatih.sen.boun@gmail.com>
2018-11-26 15:13:54 +0300

    updates

--- MVPCocoa/Test/MainController.swift
+++ MVPCocoa/Test/MainController.swift
@@ -2,12 +2,24 @@
 //  MainController.swift
 //  MVPCocoa
 //
-//  Created by Fatih Şen on 25.11.2018.
+//  Created by Fatih Şen on 26.11.2018.
 //  Copyright © 2018 Open Source. All rights reserved.
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
 
-class MainController: BaseController<MainPresenter> {
+class MainController: BaseController<MainPresenter>, MainView {
 	
+	func setPresenter(_ presenter: MainPresenter) {
+		self.presenter = presenter
+	}
+	
+	func observeTextChange() -> Observable<String> {
+		let field = UITextField()
+		return field.rx.text.asObservable()
+			.map { $0 ?? String.empty }
+	}
 }
