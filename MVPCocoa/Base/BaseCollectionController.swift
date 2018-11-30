1b97b05b29820b68e1ebc91469c07e2e411c0234
droideveloper <fatih.sen.boun@gmail.com>
2018-11-30 23:24:33 +0300

    imp table and collection controller

---
+++ MVPCocoa/Base/BaseCollectionController.swift
@@ -0,0 +1,43 @@
+//
+//  BaseCollectionController.swift
+//  MVPCocoa
+//
+//  Created by Fatih Şen on 30.11.2018.
+//  Copyright © 2018 Open Source. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class BaseCollectionController: UICollectionViewController {
+	open var presenter: Presenter! // only need this for that reason
+	
+	open override func viewDidLoad() {
+		super.viewDidLoad()
+		presenter.didLoad()
+	}
+	
+	open override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		presenter.attach()
+	}
+	
+	open override func viewWillDisappear(_ animated: Bool) {
+		presenter.detach()
+		super.viewWillAppear(animated)
+	}
+	
+	open func setUp() { }
+	
+	open func showError(_ message: String) {
+		showError(message, .alert)
+	}
+	
+	open func showError(_ message: String, completion: @escaping () -> Void) {
+		showError(message, .alert, completion)
+	}
+	
+	open func showProgress() { }
+	
+	open func hideProgress() { }
+}
