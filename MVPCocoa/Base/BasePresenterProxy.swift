ea6962c298d6b9990552559f390a8013bef283d9
droideveloper <fatih.sen.boun@gmail.com>
2019-05-01 21:44:55 +0300

    updated tempalets for base proxy

---
+++ MVPCocoa/Base/BasePresenterProxy.swift
@@ -0,0 +1,30 @@
+//
+//  BasePresenterProxy.swift
+//  MVPCocoa
+//
+//  Created by Fatih Şen on 1.05.2019.
+//  Copyright © 2019 Open Source. All rights reserved.
+//
+
+import Foundation
+
+open class BasePresenterProxy: Presenter {
+	
+	private let presenter: Presenter
+	
+	init(presenter: Presenter) {
+		self.presenter = presenter
+	}
+	
+	public func didLoad() {
+		presenter.didLoad()
+	}
+	
+	public func attach() {
+		presenter.attach()
+	}
+	
+	public func detach() {
+		presenter.detach()
+	}
+}
